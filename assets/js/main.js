df137bca5ba999a71131aae08f1f8669ead96b46
Patryk Piecuch <patrykowo@gmail.com>
2020-04-04 11:05:10 +0200

    Add toggle navigation and some more styles.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,47 @@
+function togglePreventClick(){
+    if(el = document.querySelector('.prevent-click')){
+        el.classList.remove('prevent-click');
+    }else {
+        document.querySelector('.nav-list').classList.add('prevent-click');
+    }
+}
+function fadeOut(){
+    document.querySelector('.active').classList.remove('active');
+    document.querySelector('.link-active').classList.remove('link-active');
+}
 
-function changeSection(){
+function fadeIn(str, linkEl){
+    document.getElementById(str).classList.add('active');
+    linkEl.classList.add('link-active');
+    togglePreventClick();
+}
+
+function changeSection(e){
+    const name = this.getAttribute('href').split('#').pop();
+    e.preventDefault();
+    // Prevent action when current one clicked.
     if(this.classList.contains('link-active')) return;
 
-    const destination = this.getAttribute('href').split('#');
-    const sectionToFadeIn = document.getElementById(destination[1]);
-    const sectionToFadeOut = document.querySelector('.active');
-
-    document.querySelector('.link-active').classList.remove('link-active');
-    this.classList.add('link-active');
-    sectionToFadeIn.classList.add('active');
-    sectionToFadeOut.classList.remove('active');
+    togglePreventClick();
+    fadeOut();
+    setTimeout(() => fadeIn(name, this), 500);
 }
-function pageNav(){
+function toggleNav(){
     const links = document.querySelectorAll('.nav-link');
     links.forEach((el, i) => el.addEventListener('click', changeSection));
 }
 
-
-
-
+// Set section height to the main container.
+function setBodyHeight(name = 'projects'){
+    const el = document.getElementById(name);
+    const style = window.getComputedStyle(el);
+    const height = style.getPropertyValue('height');
+    const container = document.querySelector('.main-wrapp');
+    container.style.height = height;
+}
 
 
 document.addEventListener('DOMContentLoaded', function(){
-    pageNav();
+    setBodyHeight();
+    toggleNav();
 });
