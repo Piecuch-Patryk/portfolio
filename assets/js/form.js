cb74149b5091a4b61a980325657499749738921c
Patryk Piecuch <patrykowo@gmail.com>
2020-04-07 17:53:07 +0200

    Add error and confirmation sections in the email form.

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,16 +1,39 @@
 const form = document.getElementById('form');
 form.addEventListener('submit', sendEmail);
 
+function toggleSpinner() {
+    document.getElementById('spinner').classList.contains('hidden') ? spinner.classList.remove('hidden') : spinner.classList.add('hidden');
+}
+
+function displayError() {
+    document.getElementById('error').classList.remove('hidden');
+    setTimeout(() => {
+        document.getElementById('error').classList.add('hidden');
+    }, 2000);
+}
+
+function toggleConfirmation() {
+    document.getElementById('confirmation').classList.remove('hidden');
+    setTimeout(() => {
+        document.getElementById('confirmation').classList.add('hidden');
+    }, 2000);
+}
+
 function sendEmail(e){
     const formData = new FormData(this);
     const url = 'app/app.php';
     e.preventDefault();
 
+    toggleSpinner();
+    
     fetch(url, {
         method: 'post',
         body: formData,
     })
     .then(response => response.text())
-    .then(response => console.log(response))
+    .then(response => {
+        toggleSpinner();
+        response ? toggleConfirmation() : displayError();
+    })
     .catch(error => console.error(error));
 }
